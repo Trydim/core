eb6be008624f7d14e55da5062e0f7744aeba7030
trydim <trydim85@gmail.com>
2020-11-24 10:41:17 +0300

    fix menu active

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,11 @@ const initIndex = () => {
 }
 
 const setLinkMenu = (page) => {
-  f.qA('a').forEach(n => {
+  for (let n of [...f.qA('a')]) {
     let href = n.getAttribute('href') || '';
-    if(href === page) n.parentNode.classList.add('active');
+    if(href.includes(page)) { n.parentNode.classList.add('active'); break; }
     else if (href.includes(page) && href.includes('#')) n.click();
-  })
+  }
 }
 
 const cancelFormSubmit = () => {
