919718dc08dd5d61731c1fdff2e59db4bd8ac7ff
Professional <trydim85@gmail.com>
2023-07-07 22:29:40 +0300

    remove fonts

--- root/public/src/js/react/calculator.jsx
+++ root/public/src/js/react/calculator.jsx
@@ -6,6 +6,4 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App.jsx";
 
-export default function () {
-  createRoot(document.getElementById("root")).render(<App />);
-}
+createRoot(document.getElementById("root")).render(<App />);
